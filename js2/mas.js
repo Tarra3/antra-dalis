5878924a3032aa1f345e53a8f780897b5f0970ba
Tarra3 <151025105+Tarra3@users.noreply.github.com>
2024-04-04 02:28:15 +0300

    2

---
+++ js2/mas.js
@@ -0,0 +1,41 @@
+/*
+let temp=[5, 6, 2, 3, 4,6, 7];
+
+
+let suma=0;
+let kiekis=0;
+temp.forEach( (x)=>{
+  suma+=x;
+  kiekis++;
+});
+console.log(`Vidurkis: ${suma/kiekis} `);
+*/
+
+
+
+/*
+mas.forEach((x, i)=>{
+    console.log(`${i} => ${x} `);
+});
+
+*/
+
+/*
+let men=[[1,2,3,4,1,3,4], [3,5,6,7,2,6,7], [3,2,4,5,6,7,8]];
+
+let suma=0;
+let kiekis=0;
+men.forEach( (sav)=>{
+    let savSuma=0;
+    let savKiekis=0;
+    sav.forEach((diena)=>{
+        savSuma+=diena;
+        savKiekis++;
+    });
+    suma+=savSuma;
+    kiekis+=savKiekis;
+    console.log(`Savaites vidurkis: ${savSuma / savKiekis}`);
+});
+
+console.log(` ${suma / kiekis}`);
+*/
