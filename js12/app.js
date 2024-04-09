e5af4bc0575bd1d0ea22e212985cca4ec6e4c2b6
Tarra3 <151025105+Tarra3@users.noreply.github.com>
2024-04-09 18:54:48 +0300

    js12

--- js12/app.js
+++ js12/app.js
@@ -1,80 +1,28 @@
-//zanro pasirinkimo laukas
-const zanrasSelect = document.getElementById("zanras");
-const rodytiButton = document.getElementById("rodyti");
-const rezultatai = document.getElementById("rezultatai");
-const loading = document.getElementById("loading");
-
-//parametrai f=jai fetch(prisijungimo duomenys)
-const req = {
-    headers: {
-        Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNDNhMTcxMGNkMDc4NTU2NWM1MmM4YjQxYjU2YWYwYiIsInN1YiI6IjY2MGUxOTFiZGE5ZWYyMDE3ZDU4YmRjMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wCP5A-AExFKtZPZAzK4yr5Nh4f8h0jHhZmaksgY4C2w`
-    }
-}
-
-//uzkrauname visus zanrus
-fetch(`https://api.themoviedb.org/3/genre/movie/list?language=en`, req)
+//Pradedamas krovimas
+fetch("https://restcountries.com/v3.1/name/cuba")
     .then((response) => {
-        //gauta info konvertuojame i JSON
         return response.json();
     })
     .then((data) => {
-        console.log(data);
-        data.genres.forEach((g) => {
-            const o = document.createElement("option");
-            o.textContent = g.name;
-            o.value = g.id;
-            zanrasSelect.appendChild(o);
-
-        })
-    });
-
-
-//filmu pagal zanra uzkrovimas
-const rodytiFilmusPagalZanra = () => {
-    const genreId = zanrasSelect.value;
-    loading.style.display = "block";
-    fetch(`https://api.themoviedb.org/3/discover/movie?language=lt&with_genres=${genreId}`, req)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);
-            rezultatai.innerHTML = '';
-            //jei serveris grazina rezult, bet rezult yra neigiamas,kai data success egzistuoja:
-            if (data.success !== undefined) {
-                let e = new Error(`zanras nerastas`);
-                e.name = "nerasta";
-                //komanda throw lygiia kaip ir return-nutraukia vykdyma
-                throw e;
-
-            }
-            //jei nebuvo klaidos bus tesiamas
-
-            data.results.forEach((m) => {
-                const li = document.createElement("li");
-                li.className = "list-group-item";
-                li.textContent = m.title;
-
-                rezultatai.appendChild(li);
-
-            })
-            loading.style.display = "none";
-        })
-        //kai ivyksta klaida vykdome f-ja:
-        .catch((e) =>) {
-        console.log(`klaida: ${e}`);
-        loading.style.display = "none";
-        //parodome pranesima apie klaida
-        pranesimas.innerHTML = `klaida: ${e}`;
-        if (e.name == "typeError") {
-            pranesimas.innerHTML = `klaida, serveris neveikia`; {
-            }else if (e.name == `nerastas`) {
-
-            }
+        console.log("Valstybė:");
+        console.log(data[0].name.common);
+        if (!data[0].borders) {
+            //throw new Error('Šalis kaimynų neturi')
+            return fetch(`https://restcountries.com/v3.1/alpha?codes=`)
+        } else {
+            return fetch(`https://restcountries.com/v3.1/alpha?codes=${data[0].borders.join(',')}`)
         }
-    }
-};
 
+    })
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log("Kaimynės:");
+        data.forEach((c) => {
+            console.log(c.name.common);
+        })
+    })
+    .catch(() => {
 
-rodytiButton.onclick = rodytiFilmusPagalZanra;
-
+    })
